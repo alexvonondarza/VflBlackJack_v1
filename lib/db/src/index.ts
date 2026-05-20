37005e4555a1b84678b54e2f36059b0edd3e1ce4
alexvonondarza <138570612+alexvonondarza@users.noreply.github.com>
2026-05-20 14:35:35 +0200

    Update index.ts

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -1,4 +1,20 @@
+import pg from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
+import * as schema from "./schema/index.js";
+
+const { Pool } = pg;
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle(pool, { schema });
+
+export * from "./schema/index.js";import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
 export * from "./schema/index.js";
 
